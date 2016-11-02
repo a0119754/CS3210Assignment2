db13329e739b6fdd1c8a803dd43919d321e14f80
Wei Liang <a0119754@nus.edu.sg>
2016-11-02 17:19:47 +0800

    Update SETL_par.c

--- SETL_par.c
+++ SETL_par.c
@@ -524,15 +524,16 @@ char** allocateSquareMatrix( int size, char defaultValue )
 
 void printSquareMatrix( char** matrix, int size )
 {
-    int i,j;
+	int i,j;
     
-    for (i = 0; i < size; i++){
-        for (j = 0; j < size; j++){
-            printf("%c", matrix[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
+	for (i = 0; i < size; i++) {
+		print("%d", i);
+		for (j = 0; j < size; j++) {
+			printf("%c", matrix[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
 
 void freeSquareMatrix( char** matrix )
