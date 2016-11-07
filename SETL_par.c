594bc00703548556a6095dc024c6d1480fd8a1d0
Wei Liang <a0119754@nus.edu.sg>
2016-11-07 19:01:50 +0800

    Update SETL_par.c

--- SETL_par.c
+++ SETL_par.c
@@ -289,7 +289,7 @@ int main( int argc, char** argv)
 				start = ((k == 0) || (((i - 1) % 4) < k)) ? ((l <= m) ? (l * (n + 1)) : (l * n + m)) : ((l <= m2) ? (l * (n2 + 1)) : (l * n2 + m2));
 				partSize = ((k == 0) || (((i - 1) % 4) < k)) ? (n + ((l < m) ? 1 : 0)) : (n2 + ((l < m2) ? 1 : 0));
 				if (debug) printf("Master is sending world (iter %d) to slave %d out of %d processes, from start = %d at partSize = %d\n", iter, i, processes, start, partSize);
-				MPI_Send(&(curW[start][0]), size * partSize, MPI_CHAR, i, 4 + iter, MPI_COMM_WORLD);
+				MPI_Send(&(curW[start + 1][0]), (size + 2) * partSize, MPI_CHAR, i, 4 + iter, MPI_COMM_WORLD);
 				//if (debug) printf("Master has sent world (iter %d) to slave %d out of %d processes\n", iter, i, processes);
 			}
 			
