c1a4912e7b6a7d83d5b1740e341ba3de566c94e4
Wei Liang <a0119754@nus.edu.sg>
2016-11-07 19:39:22 +0800

    Update SETL_par.c

--- SETL_par.c
+++ SETL_par.c
@@ -372,6 +372,7 @@ int main( int argc, char** argv)
 					MPI_Send(&omg, 1, MPI_INT, rank + 4, 0, MPI_COMM_WORLD);
 					if (omg > 0) cur = list->tail->next;
 					for (i = 0; i < omg; i++, cur = cur->next) {
+						if (rank == 4) printf("RANK 4: KIBOU OU HOPE blah blah (%d)\n", i);
 						MPI_Send(&(cur->iteration), 1, MPI_INT, 0, 5 + iter + i, MPI_COMM_WORLD);
 						MPI_Send(&(cur->row), 1, MPI_INT, 0, 5 + iter + i, MPI_COMM_WORLD);
 						MPI_Send(&(cur->col), 1, MPI_INT, 0, 5 + iter + i, MPI_COMM_WORLD);
